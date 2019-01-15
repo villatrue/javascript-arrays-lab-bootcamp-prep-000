cd8217ab930db1fe173b93cc5ba18a68679bcfd7
Sean Carter <scarter0313@gmail.com>
2019-01-15 01:33:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name){
   return kittens.push(name)
 }
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift
+}
